64676512df63c217150d14d44a49f6aa40551d43
M <alwaysdazed1337@gmail.com>
2025-04-28 22:01:53 +0200

    alembic/env.py updated

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from app.database import Base
 from app.config import settings
-from app.models.blogger import Blogger # noqa
+from app.models import Blogger, Campaign, Company, Integration # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
